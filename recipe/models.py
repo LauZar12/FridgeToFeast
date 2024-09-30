36c9fa91416e950840da57c628de0ce2779d2605
Moisés Arrieta <moisesdavidarh@gmail.com>
2024-09-30 12:49:23 -0500

    Agregado modelo Ingredient para normalizar los datos de ingredientes

--- recipe/models.py
+++ recipe/models.py
@@ -1,19 +1,27 @@
 from django.db import models
 from django.conf import settings
 
+
+# patrón de normalización de modelos
+
+# Separar Ingredientes de Recetas: se creó una nueva clase Ingredient que representará los ingredientes de forma independiente.
+
+# Establecer una Relación Muchos-a-Muchos: Dado que una receta puede tener muchos ingredientes y un ingrediente puede estar en muchas recetas,
+# utilizaremos una relación muchos-a-muchos
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
-    """
-    related_name='recipes': Este argumento en 
-    el campo ForeignKey significa que puedes acceder a 
-    todas las recetas de un usuario usando user.recipes.all() 
-    si user es una instancia del modelo User.
-    """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recipes')
     title = models.CharField(max_length=255)
     description = models.TextField()
-    parameters = models.TextField()  
-    generation_date = models.DateField(auto_now_add=True)  # Esto automáticamente establecerá la fecha cuando la receta se cree
-    image = models.URLField(blank=True, null=True)  
+    ingredients = models.ManyToManyField(Ingredient)  # Relación muchos-a-muchos con Ingredient
+    generation_date = models.DateField(auto_now_add=True)
+    image = models.URLField(blank=True, null=True)
     cuisine = models.TextField()
     number_of_portions = models.TextField()
     favourite_state = models.BooleanField(default=False)
@@ -23,16 +31,10 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         from django.urls import reverse
-
         return reverse("recipe:show", kwargs={"pk": self.pk})
 
     @property
     def short_description(self):
-        preview_length = 150
         import ast
-        description = ' '.join(ast.literal_eval(self.description))
-        try:
-            description = description[:preview_length]
-        except IndexError:
-            pass
-        return description
+        description_list = ast.literal_eval(self.description) 
+        return ' '.join(description_list)[:150]
