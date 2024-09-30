f1c13d679c76e51cea96bf14f33d99fadd209dda
Moisés Arrieta <moisesdavidarh@gmail.com>
2024-09-30 12:50:18 -0500

    Refactorizado modelo Recipe para incluir relación ManyToMany con Ingredient

--- recipe/models.py
+++ recipe/models.py
@@ -26,15 +26,4 @@ class Recipe(models.Model):
     number_of_portions = models.TextField()
     favourite_state = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        from django.urls import reverse
-        return reverse("recipe:show", kwargs={"pk": self.pk})
-
-    @property
-    def short_description(self):
-        import ast
-        description_list = ast.literal_eval(self.description) 
-        return ' '.join(description_list)[:150]
+    
